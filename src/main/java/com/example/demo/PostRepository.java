3e5933b15d7fd85caa8acf95f8aced81ba25a8ae
Aswinkumar20 <aswinkumar11999@gmail.com>
2022-11-13 02:08:50 +0530

    second commit

--- src/main/java/com/example/demo/PostRepository.java
+++ src/main/java/com/example/demo/PostRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.Post;
+
 
 public interface PostRepository extends JpaRepository<Post,Long> {
 
